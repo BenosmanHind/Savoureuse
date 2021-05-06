93f350e165387f8e792f5b4e1766d0b6cec584dd
YasminaDALIYOUCEF <82341552+YasminaDALIYOUCEF@users.noreply.github.com>
2021-05-07 00:13:19 +0100

    Update profil-cuisinier.blade.php

--- resources/views/profil-cuisinier.blade.php
+++ resources/views/profil-cuisinier.blade.php
@@ -45,8 +45,10 @@
                             <div class="user-content">
                                 <a href="javascript:void(0)"><img src="dashboard/plugins/images/users/genu.jpg"
                                         class="thumb-lg img-circle" alt="img"></a>
-                                <h4 class="text-white mt-2">User Name</h4>
-                                <h5 class="text-white mt-2">info@myadmin.com</h5>
+
+                                <h4 class="text-white mt-2">{{ old('name', Auth::user()->name) }}</h4>
+                                <h5 class="text-white mt-2">{{ old('email', Auth::user()->email) }}</h5>
+
                             </div>
                         </div>
                     </div>
@@ -68,53 +70,49 @@
             <div class="col-lg-8 col-xlg-9 col-md-12">
                 <div class="card">
                     <div class="card-body">
-                        <form class="form-horizontal form-material">
+                        <form class="form-horizontal form-material" action="{{url('/profil-cuisinier/'.Auth::user()->id)}}" method="post">
+                             <input type="hidden" name="_method" value="PUT">
+                               @csrf
+    
                             <div class="form-group mb-4">
                                 <label class="col-md-12 p-0">Full Name</label>
-                                <div class="col-md-12 border-bottom p-0">
-                                    <input type="text" placeholder="Johnathan Doe"
-                                        class="form-control p-0 border-0"> </div>
+                                
+                              <div class="col-md-12 border-bottom p-0">
+                                    <input type="text" value="{{ old('name', Auth::user()->name) }}" class="form-control @error('name') is-invalid @enderror" name="name"  required class="form-control p-0 border-0" >
+                               
+                                         </div>
+
+
                             </div>
+
+                             <div class="form-group mb-4">
+                                        <label class="col-md-12 p-0">Date de naissance</label>
+                                        <div class="col-md-12 border-bottom p-0">
+                                            <input type="date"  value="{{ old('date_naissance', Auth::user()->date_naissance) }}"
+                                                   class="form-control @error('date_naissance') is-invalid @enderror" name="date_naissance"
+                                                        required  class="form-control p-0 border-0" > 
+
+                                          </div>
+                                    </div>  
+
                             <div class="form-group mb-4">
                                 <label for="example-email" class="col-md-12 p-0">Email</label>
                                 <div class="col-md-12 border-bottom p-0">
-                                    <input type="email" placeholder="johnathan@admin.com"
-                                        class="form-control p-0 border-0" name="example-email"
-                                        id="example-email">
-                                </div>
+                                <input type="email" value="{{ old('email', Auth::user()->email) }}"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"  required >
+
+                     
+                                    </div>
                             </div>
                             <div class="form-group mb-4">
                                 <label class="col-md-12 p-0">Password</label>
                                 <div class="col-md-12 border-bottom p-0">
-                                    <input type="password" value="password" class="form-control p-0 border-0">
+                                    <input type="password"  class="form-control p-0 border-0"
+                                        class="form-control @error('password') is-invalid @enderror" name="password">
                                 </div>
                             </div>
-                            <div class="form-group mb-4">
-                                <label class="col-md-12 p-0">Phone No</label>
-                                <div class="col-md-12 border-bottom p-0">
-                                    <input type="text" placeholder="123 456 7890"
-                                        class="form-control p-0 border-0">
-                                </div>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label class="col-md-12 p-0">Message</label>
-                                <div class="col-md-12 border-bottom p-0">
-                                    <textarea rows="5" class="form-control p-0 border-0"></textarea>
-                                </div>
-                            </div>
-                            <div class="form-group mb-4">
-                                <label class="col-sm-12">Select Country</label>
+                            
 
-                                <div class="col-sm-12 border-bottom">
-                                    <select class="form-select shadow-none p-0 border-0 form-control-line">
-                                        <option>London</option>
-                                        <option>India</option>
-                                        <option>Usa</option>
-                                        <option>Canada</option>
-                                        <option>Thailand</option>
-                                    </select>
-                                </div>
-                            </div>
                             <div class="form-group mb-4">
                                 <div class="col-sm-12">
                                     <button class="btn btn-success">Update Profile</button>
